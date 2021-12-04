114a8b857831597ccd72e89a04cbb0ed0ad2e531
Stefan Vlajkovic <vlajkovicstefan01@gmail.com>
2021-12-04 13:11:31 +0100

    Dodati podaci u bazu podataka i u sqlquery.

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -75,3 +75,76 @@ CREATE TABLE treninzi(
 	constraint trening_instruktor_fk foreign key (Instruktor_JMBG) references instruktori(JMBG),
 	constraint trening_polaznik_fk foreign key (Polaznik_JMBG) references polaznici(JMBG),
 );
+
+-- Izmena postojecih podataka 
+alter table treninzi
+alter column vreme varchar(10)
+
+drop table korisnici
+drop table adrese
+drop table polaznici
+drop table instruktori
+drop table centri
+drop table treninzi
+
+-- Dodavanje vrednosti
+insert into centri values
+	(1, 'Max Fitness', 2, 'True')
+
+insert into adrese values
+	(0, '', '', '','', 'False')
+insert into adrese values
+	(1, 'Luke Stevica', '111/A', 'Loznica','Srbija', 'True')
+insert into adrese values
+	(2, 'Novosadska', '115', 'Novi Sad','Srbija', 'True')
+
+insert into korisnici values
+	('', '', '0000000000000', 'MUSKI', 0, '@gmail.com', '0', 'POLAZNIK', 'False')
+insert into korisnici values
+	('Stefan', 'Vlajkovic', '1234567891234', 'MUSKI', 1, 'vlajkovic@gmail.com', '12345', 'ADMINISTRATOR', 'True')
+insert into korisnici values
+	('Janko', 'Jankovic', '1234567810101', 'MUSKI', 2, 'jankovic@gmail.com', '12345', 'POLAZNIK', 'True')
+insert into korisnici values
+	('Nikola', 'Nikolic', '9876543219876', 'MUSKI', 1, 'nikolic@gmail.com', '12345', 'INSTRUKTOR', 'True')
+insert into korisnici values
+	('Marko', 'Markovic', '1122334455667', 'MUSKI', 2, 'markovic@gmail.com', '12345', 'INSTRUKTOR', 'True')
+insert into korisnici values
+	('Darko', 'Panic', '1234567899999', 'MUSKI', 2, 'panic@gmail.com', '12345', 'POLAZNIK', 'True')
+-- Test
+insert into korisnici values
+	('Test', 'Test', '1111122222333', 'MUSKI', 2, 'panic@gmail.com', '12345', 'ADMINISTRATOR', 'True')
+
+insert into instruktori values
+	('Marko', 'Markovic', '1122334455667', 'MUSKI', 2, 'markovic@gmail.com', '12345', 'INSTRUKTOR', 'True')
+insert into instruktori values
+	('Nikola', 'Nikolic', '9876543219876', 'MUSKI', 1, 'nikolic@gmail.com', '12345', 'INSTRUKTOR', 'True')
+
+insert into polaznici values
+	('', '', '0000000000000', 'MUSKI', 0, '@gmail.com', '0', 'POLAZNIK', 'False')
+insert into polaznici values
+	('Janko', 'Jankovic', '1234567810101', 'MUSKI', 2, 'jankovic@gmail.com', '12345', 'POLAZNIK', 'True')
+insert into polaznici values
+	('Darko', 'Panic', '1234567899999', 'MUSKI', 2, 'panic@gmail.com', '12345', 'POLAZNIK', 'True')
+
+insert into treninzi values
+	(1, '20.11.2021', '14:00', 60, 'REZERVISAN', '9876543219876', '1234567810101', 'True')
+insert into treninzi values
+	(2, '21.11.2021', '15:00', 120, 'REZERVISAN', '9876543219876', '1234567810101', 'True')
+insert into treninzi values
+	(3, '22.11.2021', '16:00', 60, 'REZERVISAN', '1122334455667', '1234567899999', 'True')
+insert into treninzi values
+	(4, '23.11.2021', '17:00', 60, 'SLOBODAN', '9876543219876', '0000000000000', 'True')
+insert into treninzi values
+	(5, '24.11.2021', '18:00', 60, 'SLOBODAN', '1122334455667', '0000000000000', 'True')
+
+-- Pregled
+select *
+from korisnici
+select *
+from polaznici
+select *
+from instruktori
+select *
+from adrese
+select *
+from treninzi
